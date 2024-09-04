bac6a18f7e26dfebd2561f55d8924c162f7af974
User <User@FUAD>
2024-09-03 20:45:17 -0400

    Add project files.

---
+++ GenericValueCollectionDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericValueCollectionDemo
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            var vc = new ValueCollection<int>();
+            vc.AddItem(10);
+            vc.AddItem(5);
+            vc.AddItem(20);
+
+            foreach (var item in vc.GetSortedItemsDescending())
+            {
+                Console.WriteLine(item);
+            }
+             
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GenericValueCollectionDemo/ValueCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericValueCollectionDemo
+{
+    public class ValueCollection<T> where T : struct
+    {
+        private List<T> items = new List<T>();
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public T GetItem(int index)
+        {
+            if (index >= 0 && index < items.Count)
+                return items[index];
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+
+        public IEnumerable<T> GetSortedItemsDescending()
+        {
+            return items.OrderByDescending(x => x);
+        }
+    }
+}
